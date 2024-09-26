a8d73271de483274d489915795b0319478de5ed3
Eric Gallager <egall@gwmail.gwu.edu>
2024-09-25 21:56:40 -0400

    Update content_view.js
    more `eslint` autofixes

--- app/assets/javascripts/app/views/content_view.js
+++ app/assets/javascripts/app/views/content_view.js
@@ -5,61 +5,60 @@ app.views.Content = app.views.Base.extend({
     "click .expander": "expandPost"
   },
 
-  presenter : function(){
+  presenter: function() {
     return _.extend(this.defaultPresenter(), {
-      text : app.helpers.textFormatter(this.model.get("text"), this.model.get("mentioned_people")),
-      largePhoto : this.largePhoto(),
-      smallPhotos : this.smallPhotos(),
-      isReshare : this.model.get("post_type") === "Reshare"
+      text: app.helpers.textFormatter(this.model.get("text"), this.model.get("mentioned_people")),
+      largePhoto: this.largePhoto(),
+      smallPhotos: this.smallPhotos(),
+      isReshare: this.model.get("post_type") === "Reshare"
     });
   },
 
-
-  largePhoto : function() {
+  largePhoto: function() {
     var photos = this.model.get("photos");
     if (!photos || photos.length === 0) { return false; }
     return photos[0];
   },
 
-  smallPhotos : function() {
+  smallPhotos: function() {
     var photos = this.model.get("photos");
     if (!photos || photos.length < 2) { return false; }
     return photos.slice(1); // remove first photo as it is already shown as largePhoto
   },
 
   expandPost: function(evt) {
-    var el = $(this.el).find('.collapsible');
-    el.removeClass('collapsed').addClass('opened');
-    el.animate({'height':el.data('orig-height')}, 550, function() {
-      el.css('height','auto');
+    var el = $(this.el).find(".collapsible");
+    el.removeClass("collapsed").addClass("opened");
+    el.animate({"height": el.data("orig-height")}, 550, function() {
+      el.css("height", "auto");
     });
     $(evt.currentTarget).hide();
   },
 
-  collapseOversized : function() {
-    var collHeight = 200
-      , elem = this.$(".collapsible")
-      , oembed = elem.find(".oembed")
-      , opengraph = elem.find(".opengraph")
-      , addHeight = 0;
-    if($.trim(oembed.html()) !== "") {
+  collapseOversized: function() {
+    var collHeight = 200,
+        elem = this.$(".collapsible"),
+        oembed = elem.find(".oembed"),
+        opengraph = elem.find(".opengraph"),
+        addHeight = 0;
+    if ($.trim(oembed.html()) !== "") {
       addHeight += oembed.height();
     }
-    if($.trim(opengraph.html()) !== "") {
+    if ($.trim(opengraph.html()) !== "") {
       addHeight += opengraph.height();
     }
 
     // only collapse if height exceeds collHeight+20%
-    if( elem.height() > ((collHeight*1.2)+addHeight) && !elem.is(".opened") ) {
-      elem.data("orig-height", elem.height() );
+    if (elem.height() > ((collHeight * 1.2) + addHeight) && !elem.is(".opened")) {
+      elem.data("orig-height", elem.height());
       elem
-        .height( Math.max(collHeight, addHeight) )
+        .height(Math.max(collHeight, addHeight))
         .addClass("collapsed")
         .append(
-        $('<div />')
-          .addClass('expander')
-          .text( Diaspora.I18n.t("show_more") )
-      );
+          $("<div />")
+            .addClass("expander")
+            .text(Diaspora.I18n.t("show_more"))
+        );
     }
   },
 
@@ -84,7 +83,7 @@ app.views.Content = app.views.Base.extend({
     this.$(".media-embed .thumb").bind("click", this.onVideoThumbClick);
   },
 
-  postRenderTemplate : function(){
+  postRenderTemplate: function() {
     this.bindMediaEmbedThumbClickEvent();
     _.defer(_.bind(this.collapseOversized, this));
 
@@ -95,7 +94,7 @@ app.views.Content = app.views.Base.extend({
         this.addEventListener("load", function() {
           // only fire if the top of the post is in viewport
           var rect = self.el.getBoundingClientRect();
-          if(rect.top > 0) {
+          if (rect.top > 0) {
             self.collapseOversized.call(self);
           }
         });
@@ -103,51 +102,51 @@ app.views.Content = app.views.Base.extend({
     });
 
     var photoAttachments = this.$(".photo-attachments");
-    if(photoAttachments.length > 0) {
-      new app.views.Gallery({ el: photoAttachments });
+    if (photoAttachments.length > 0) {
+      new app.views.Gallery({el: photoAttachments});
     }
   }
 });
 
 app.views.StatusMessage = app.views.Content.extend({
-  templateName : "status-message"
+  templateName: "status-message"
 });
 
 app.views.ExpandedStatusMessage = app.views.StatusMessage.extend({
-  postRenderTemplate : function(){
+  postRenderTemplate: function() {
     this.bindMediaEmbedThumbClickEvent();
 
     var photoAttachments = this.$(".photo-attachments");
-    if(photoAttachments.length > 0) {
-      new app.views.Gallery({ el: photoAttachments });
+    if (photoAttachments.length > 0) {
+      new app.views.Gallery({el: photoAttachments});
     }
   }
 });
 
 app.views.Reshare = app.views.Content.extend({
-  templateName : "reshare"
+  templateName: "reshare"
 });
 
 app.views.OEmbed = app.views.Base.extend({
-  templateName : "oembed",
-  events : {
+  templateName: "oembed",
+  events: {
     "click .thumb": "showOembedContent"
   },
 
-  presenter:function () {
+  presenter: function() {
     var o_embed_cache = this.model.get("o_embed_cache");
-    if(o_embed_cache) {
-      var typemodel = { rich: false, photo: false, video: false, link: false };
+    if (o_embed_cache) {
+      var typemodel = {rich: false, photo: false, video: false, link: false};
       typemodel[o_embed_cache.data.type] = true;
       o_embed_cache.data.types = typemodel;
     }
     return _.extend(this.defaultPresenter(), {
-      o_embed_html : app.helpers.oEmbed.html(o_embed_cache)
+      o_embed_html: app.helpers.oEmbed.html(o_embed_cache)
     });
   },
 
-  showOembedContent : function (evt) {
-    if( $(evt.target).is('a') ) return;
+  showOembedContent: function(evt) {
+    if ($(evt.target).is("a")) { return; }
     var clickedThumb = false;
     if ($(evt.target).hasClass(".thumb")) {
       clickedThumb = $(evt.target);
@@ -155,7 +154,7 @@ app.views.OEmbed = app.views.Base.extend({
       clickedThumb = $(evt.target).parent(".thumb");
     }
     var insertHTML = $(app.helpers.oEmbed.html(this.model.get("o_embed_cache")));
-    var paramSeparator = ( /\?/.test(insertHTML.attr("src")) ) ? "&" : "?";
+    var paramSeparator = (/\?/.test(insertHTML.attr("src"))) ? "&" : "?";
     insertHTML.attr("src", insertHTML.attr("src") + paramSeparator + "autoplay=1&wmode=opaque");
     if (clickedThumb) {
       insertHTML.attr("width", clickedThumb.width());
@@ -166,7 +165,7 @@ app.views.OEmbed = app.views.Base.extend({
 });
 
 app.views.OpenGraph = app.views.Base.extend({
-  templateName : "opengraph",
+  templateName: "opengraph",
 
   events: {
     "click .video-overlay": "loadVideo"
@@ -178,8 +177,8 @@ app.views.OpenGraph = app.views.Base.extend({
 
   truncateDescription: function() {
     // truncate opengraph description to 250 for stream view
-    if(this.model.has('open_graph_cache')) {
-      var ogdesc = this.model.get('open_graph_cache');
+    if (this.model.has("open_graph_cache")) {
+      var ogdesc = this.model.get("open_graph_cache");
       ogdesc.description = app.helpers.truncate(ogdesc.description, 250);
     }
   },
@@ -192,7 +191,7 @@ app.views.OpenGraph = app.views.Base.extend({
 });
 
 app.views.SPVOpenGraph = app.views.OpenGraph.extend({
-  truncateDescription: function () {
+  truncateDescription: function() {
     // override with nothing
   }
 });
